54387d9b1316a1e978a8a9bb2d9c87e309976bf2
vitalii-moroz <morozvitalii2403@gmail.com>
2023-11-13 19:57:48 +0200

    change card grid so it's 3x4 and change game logic

--- main.js
+++ main.js
@@ -4,30 +4,30 @@ const newGameBtn = document.querySelector(".new-game-btn");
 const cardsContainer = document.querySelector(".cards-container");
 
 const images = [
-"airplane-svgrepo-com.svg",
-"amphora-svgrepo-com.svg",
-"antenna-bus-svgrepo-com.svg",
-"articulated-lorry-svgrepo-com.svg",
-"basketball-svgrepo-com.svg",
-"bed-svgrepo-com.svg",
-"bowling-svgrepo-com.svg",
-"briefcase-svgrepo-com.svg",
-"cake-svgrepo-com.svg",
-"car-svgrepo-com.svg",
-"carrot-svgrepo-com.svg",
-"chicken-svgrepo-com.svg",
-"dove-svgrepo-com.svg",
-"flower-rose-svgrepo-com.svg",
-"freighter-svgrepo-com.svg",
-"helicopter-svgrepo-com.svg",
-"hen-svgrepo-com.svg",
-"hot-air-balloon-svgrepo-com.svg",
-"house-svgrepo-com.svg",
-"ladybug-svgrepo-com.svg",
-"paper-plane-svgrepo-com.svg",
-"scooter-scooter-svgrepo-com.svg",
-"scraper-svgrepo-com.svg"
-]
+  "airplane-svgrepo-com.svg",
+  "amphora-svgrepo-com.svg",
+  "antenna-bus-svgrepo-com.svg",
+  "articulated-lorry-svgrepo-com.svg",
+  "basketball-svgrepo-com.svg",
+  "bed-svgrepo-com.svg",
+  "bowling-svgrepo-com.svg",
+  "briefcase-svgrepo-com.svg",
+  "cake-svgrepo-com.svg",
+  "car-svgrepo-com.svg",
+  "carrot-svgrepo-com.svg",
+  "chicken-svgrepo-com.svg",
+  "dove-svgrepo-com.svg",
+  "flower-rose-svgrepo-com.svg",
+  "freighter-svgrepo-com.svg",
+  "helicopter-svgrepo-com.svg",
+  "hen-svgrepo-com.svg",
+  "hot-air-balloon-svgrepo-com.svg",
+  "house-svgrepo-com.svg",
+  "ladybug-svgrepo-com.svg",
+  "paper-plane-svgrepo-com.svg",
+  "scooter-scooter-svgrepo-com.svg",
+  "scraper-svgrepo-com.svg",
+];
 
 cardsContainer.classList.add("disabled-cards");
 
@@ -39,32 +39,29 @@ cardElems.forEach((card) => {
 });
 
 function getRandomImages() {
-  const randomImages = []
-  while(randomImages.length<guessImgElems.length) {
+  const randomImages = [];
+  while (randomImages.length < guessImgElems.length) {
     const randomImg = images[Math.floor(Math.random() * images.length)];
     console.log(randomImg);
 
-    if(!randomImages.includes(randomImg)) {
-      randomImages.push(randomImg, randomImg, randomImg);
-
-    
+    if (!randomImages.includes(randomImg)) {
+      randomImages.push(randomImg, randomImg);
     }
   }
   return randomImages;
 }
 
-function shuffleRandomImages(randomImages){
-    const shuffledArr = randomImages.sort(() => Math.random() - 0.5);
+function shuffleRandomImages(randomImages) {
+  const shuffledArr = randomImages.sort(() => Math.random() - 0.5);
   return shuffledArr;
 }
 
-
-function addRandomImagesToCards(imgElems){
+function addRandomImagesToCards(imgElems) {
   const shuffledImages = shuffleRandomImages(getRandomImages());
   imgElems.forEach((imgElem, index) => {
     imgElem.src = `./images/images_to_guess/${shuffledImages[index]}`;
     console.log(imgElem.src);
-  })
+  });
 }
 
 function showBack() {
@@ -79,32 +76,27 @@ function showFront() {
 }
 
 function restartGame() {
-addRandomImagesToCards(guessImgElems)
-  showFront()
-newGameBtn.disabled = true;
-newGameBtn.classList.add("disabled-btn");
-if(!cardsContainer.classList.contains("disabled-cards")) {
-  cardsContainer.classList.add("disabled-cards");
-}
-setTimeout(() => {
-  showBack()
-  newGameBtn.disabled = false;
-  if(cardsContainer.classList.contains("disabled-cards")) {
-    cardsContainer.classList.remove("disabled-cards");
+  addRandomImagesToCards(guessImgElems);
+  showFront();
+  newGameBtn.disabled = true;
+  newGameBtn.classList.add("disabled-btn");
+  if (!cardsContainer.classList.contains("disabled-cards")) {
+    cardsContainer.classList.add("disabled-cards");
   }
-  newGameBtn.classList.remove("disabled-btn");
-  newGameBtn.classList.add("cursor-pointer");
-  cardElems.forEach((card) => {
-    card.classList.add("cursor-pointer");
-  });
-
+  setTimeout(() => {
+    showBack();
+    newGameBtn.disabled = false;
+    if (cardsContainer.classList.contains("disabled-cards")) {
+      cardsContainer.classList.remove("disabled-cards");
+    }
+    newGameBtn.classList.remove("disabled-btn");
+    newGameBtn.classList.add("cursor-pointer");
+    cardElems.forEach((card) => {
+      card.classList.add("cursor-pointer");
+    });
   }, 2000);
 }
 
-
-
 newGameBtn.addEventListener("click", () => {
-  restartGame()
-}
-)
-
+  restartGame();
+});
